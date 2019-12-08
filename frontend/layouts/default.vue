99998d46d87361f99ac1de20e055ed0e9e500ed5
Milad Rastian <eslashmili@gmail.com>
2019-12-08 20:39:56 +0100

    implement darkmode switch

--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
   <v-app light>
-
+ 
     <v-content>
       <v-container>
         <nuxt />
@@ -8,7 +8,9 @@
     </v-content>
 
     <v-footer app>
-      <span>footer</span>
+      <span>footer
+       <v-switch label="Dark Mode" @change="toggleDarkMode()"/>
+      </span>
     </v-footer>
   </v-app>
 </template>
@@ -17,6 +19,11 @@
 export default {
   created() {
     this.$store.dispatch('authProvider/all')
+  },
+  methods: {
+    toggleDarkMode() {
+      this.$vuetify.theme.dark = !this.$vuetify.theme.dark
+    }
   }
 }
 </script>
--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -80,6 +80,15 @@ export default {
           warning: colors.amber.base,
           error: colors.deepOrange.accent4,
           success: colors.green.accent3
+        },
+        light: {
+          primary: colors.blue.lighten2,
+          accent: colors.grey.lighten3,
+          secondary: colors.amber.lighten3,
+          info: colors.teal.lighten1,
+          warning: colors.amber.base,
+          error: colors.deepOrange.accent4,
+          success: colors.green.accent3
         }
       }
     }
