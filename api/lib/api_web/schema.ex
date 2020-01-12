fced69dc5a373ed03b7a67ed9eeb9bbea865da6d
Ioana Rosu <ioana.rosu@solarisbank.de>
2020-01-12 19:02:02 +0100

    Add auth_providers query field and object.

--- api/lib/api_web/schema.ex
+++ api/lib/api_web/schema.ex
@@ -1,6 +1,19 @@
 defmodule JuntoApiWeb.Schema do
   use Absinthe.Schema
 
+  object :auth_provider do
+    field(:auth_type, :string)
+    field(:client_id, :string)
+    field(:scope, :string)
+    field(:auth_url, :string)
+  end
+
   query do
+    @desc "Get a list of :auth_providers"
+    field :auth_providers, list_of(:auth_provider) do
+      resolve(fn _parent, _args, _resolution ->
+        {:ok, []}
+      end)
+    end
   end
 end
