f4e8103bb56d8c408a709c569c4dfee44efef96a
Hugo Duksis <duksis@gmail.com>
2020-03-15 13:03:59 +0100

    Update release config

--- api/config/releases.exs
+++ api/config/releases.exs
@@ -1,10 +1,15 @@
 import Config
 
-database_url = System.get_env("DATABASE_URL")
+database_url = System.fetch_env!("DATABASE_URL")
+secret_key_base = System.fetch_env!("SECRET_KEY_BASE")
 
 config :api, JuntoApi.Repo,
   url: database_url,
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
+
+config :api, JuntoApiWeb.Endpoint,
+  http: [:inet6, port: String.to_integer(System.get_env("PORT") || "4000")],
+  secret_key_base: secret_key_base
 
 config :ueberauth, Ueberauth.Strategy.Github.OAuth,
   client_id: System.get_env("GITHUB_CLIENT_ID"),
@@ -13,3 +18,5 @@ config :ueberauth, Ueberauth.Strategy.Github.OAuth,
 config :ueberauth, Ueberauth.Strategy.Twitter.OAuth,
   client_id: System.get_env("TWITTER_CONSUMER_KEY"),
   client_secret: System.get_env("TWITTER_CONSUMER_SECRET")
+
+config :api, JuntoApiWeb.Endpoint, server: true
