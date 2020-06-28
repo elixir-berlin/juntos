2044f7540d621a59e7c982253aee41aa54658660
Milad Rastian <milad@high-mobility.com>
2020-06-28 06:30:39 +0200

    divide dependency based on dev need

--- mix.exs
+++ mix.exs
@@ -38,13 +38,14 @@ defmodule Juntos.MixProject do
       {:ecto_sql, "~> 3.4"},
       {:postgrex, ">= 0.0.0"},
       {:phoenix_html, "~> 2.11"},
-      {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:phoenix_live_dashboard, "~> 0.2.0"},
       {:telemetry_metrics, "~> 0.4"},
       {:telemetry_poller, "~> 0.4"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
+      # dev
+      {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:credo, "~> 1.4", only: [:dev, :test], runtime: false}
     ]
   end
