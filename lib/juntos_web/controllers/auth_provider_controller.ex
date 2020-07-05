ec16d2b0b00d06dc04488a45b87984ff91eb46a7
Milad Rastian <miladrastian@gmail.com>
2020-07-05 14:50:29 +0200

    if auth recrod is not assigned, send user to new user form

--- lib/juntos_web/controllers/auth_provider_controller.ex
+++ lib/juntos_web/controllers/auth_provider_controller.ex
@@ -11,12 +11,38 @@ defmodule JuntosWeb.AuthProviderController do
   def internal_callback(conn, ueberauth_auth) do
     case Accounts.create_authorization(ueberauth_auth) do
       {:ok, authorization} ->
-        conn
-        |> put_session(:authorization_id, authorization.id)
-        |> redirect(to: "/users/new")
+        redirect_new_user(conn, authorization)
+
+      {:error,
+       %{
+         errors: [
+           provider_uid:
+             {_,
+              [constraint: :unique, constraint_name: "account_authorizations_provider_uid_index"]}
+         ]
+       } = changeset} ->
+        authorization =
+          Accounts.get_authorization_by(
+            uid: changeset.data.uid,
+            provider: changeset.data.provider
+          )
+
+        if authorization.user_id do
+          throw(:login_user)
+        else
+          redirect_new_user(conn, authorization)
+        end
 
       {:error, _changeset} ->
-        throw(:what_to_do_next?)
+        throw(:some_data_are_missing)
     end
+  end
+
+  defp redirect_new_user(conn, authorization) do
+    conn
+    |> configure_session(renew: true)
+    |> clear_session()
+    |> put_session(:authorization_id, authorization.id)
+    |> redirect(to: "/users/new")
   end
 end
--- test/juntos_web/controllers/auth_provider_controller_test.exs
+++ test/juntos_web/controllers/auth_provider_controller_test.exs
@@ -26,5 +26,33 @@ defmodule JuntosWeb.AuthProviderControllerTest do
       assert Accounts.get_authorization_by(uid: "1000", provider: :github).id == recorded_id
       assert redirected_to(conn) =~ "/users/new"
     end
+
+    test "redirects to new user page if authorization record is not assigned to any user", %{
+      conn: conn
+    } do
+      ueberauth_auth = %{
+        uid: 1000,
+        provider: :github,
+        info: %{
+          email: "foo@example.com",
+          image: "https://avatar.url",
+          nickname: "foobar",
+          name: "Foo Bar"
+        },
+        credentials: %{token: "---token---"},
+        extra: %{raw_info: %{user: %{"emails" => []}}}
+      }
+
+      _ = Accounts.create_authorization(ueberauth_auth)
+
+      conn =
+        conn
+        |> init_test_session(foo: "bar")
+        |> JuntosWeb.AuthProviderController.internal_callback(ueberauth_auth)
+
+      recorded_id = Plug.Conn.get_session(conn, :authorization_id)
+      assert Accounts.get_authorization_by(uid: "1000", provider: :github).id == recorded_id
+      assert redirected_to(conn) =~ "/users/new"
+    end
   end
 end
