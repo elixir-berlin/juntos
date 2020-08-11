75def7c074d48a614cb741dadf94b6847736da16
Milad Rastian <miladrastian@gmail.com>
2020-08-11 21:20:28 +0200

    add test for group show page

--- test/juntos_web/live/group_live_test.exs
+++ test/juntos_web/live/group_live_test.exs
@@ -2,6 +2,8 @@ defmodule JuntosWeb.GroupLiveTest do
   use JuntosWeb.ConnCase
 
   import Phoenix.LiveViewTest
+  import Juntos.MeetupsFixtures
+  import Juntos.AccountsFixtures
 
   @create_attrs %{name: "some name", slug: "some-slug"}
   @invalid_attrs %{name: nil, slug: nil}
@@ -25,6 +27,15 @@ defmodule JuntosWeb.GroupLiveTest do
       assert group = Juntos.Meetups.get_group_by(slug: @create_attrs[:slug])
       assert group.creator_id == user.id
       assert html =~ "Group created successfully"
+    end
+  end
+
+  describe "Show" do
+    test "show group page", %{conn: conn} do
+      user = user_fixture()
+      group = group_fixture(%{creator_id: user.id})
+      {:ok, _, html} = live(conn, Routes.group_show_path(conn, :show, [group.slug]))
+      assert html =~ group.name
     end
   end
 end
