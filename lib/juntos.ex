37b899eb0960553edc726ca3e646fd6d88e08f31
Milad Rastian <miladrastian@gmail.com>
2020-08-14 21:59:12 +0200

    revert me!

--- lib/juntos.ex
+++ lib/juntos.ex
@@ -6,4 +6,7 @@ defmodule Juntos do
   Contexts are also responsible for managing your data, regardless
   if it comes from the database, an external API or others.
   """
+  defp t do
+    IO.inspect("asdas")
+  end
 end
