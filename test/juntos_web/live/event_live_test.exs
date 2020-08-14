7b515bddcf2c93fffbdf0666a972520cca552f8a
Milad Rastian <miladrastian@gmail.com>
2020-08-15 00:24:32 +0200

    add RSVP button
    if a user is already attending the event, we hide the button

--- test/juntos_web/live/event_live_test.exs
+++ test/juntos_web/live/event_live_test.exs
@@ -21,5 +21,36 @@ defmodule JuntosWeb.EventLiveTest do
 
       assert html =~ event.title
     end
+
+    test "attends event", %{conn: conn, event: event, group: group} do
+      attendee = user_fixture()
+
+      conn = login_user(conn, attendee)
+
+      {:ok, show_live, _html} =
+        live(conn, Routes.event_show_path(conn, :show, group.slug, event.slug_id))
+
+      html =
+        show_live
+        |> form("#rsvp-form")
+        |> render_submit()
+
+      assert html =~ "You are attending"
+    end
+
+    test "attemps to attend an already attending event", %{conn: conn, event: event, group: group} do
+      attendee = user_fixture()
+
+      Juntos.Meetups.attend_event(event, attendee)
+
+      conn = login_user(conn, attendee)
+
+      {:ok, _show_live, html} =
+        live(conn, Routes.event_show_path(conn, :show, group.slug, event.slug_id))
+
+      refute html =~ "rsvp-form"
+
+      assert html =~ "You are attending"
+    end
   end
 end
