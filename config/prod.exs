9c1f3bbbf05c03adaead7801906b1218e93dec5c
Milad Rastian <miladrastian@gmail.com>
2020-09-20 21:24:12 +0200

    configure phoenix to server in heroku

--- config/prod.exs
+++ config/prod.exs
@@ -2,6 +2,7 @@ use Mix.Config
 
 config :juntos, JuntosWeb.Endpoint,
   url: [host: System.get_env("HOSTNAME") || "localhost", port: 80],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
